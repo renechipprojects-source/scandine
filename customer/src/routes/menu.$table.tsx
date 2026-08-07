519c162b42f0a78f03caf798fe1a09fa8b433ab3
renechipprojects-source <renechipprojects@gmail.com>
2026-08-07 22:25:58 +0530

    fix(customer): replace throw redirect with Navigate component in menu..tsx

--- customer/src/routes/menu.$table.tsx
+++ customer/src/routes/menu.$table.tsx
@@ -1,14 +1,23 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute, Navigate } from "@tanstack/react-router";
+import { useEffect } from "react";
 import { tableStore } from "@/lib/table-store";
 
 export const Route = createFileRoute("/menu/$table")({
-  beforeLoad: ({ params }) => {
-    if (params.table) {
-      const rawDigits = params.table.replace(/\D/g, "");
-      const formatted = rawDigits ? `Table ${rawDigits}` : params.table;
-      tableStore.setTableNumber(formatted);
-    }
-    throw redirect({ to: "/", replace: true });
-  },
-  component: () => null,
+  component: MenuTableRoute,
 });
+
+function MenuTableRoute() {
+  const { table } = Route.useParams();
+
+  const rawDigits = (table || "").replace(/\D/g, "");
+  const formatted = rawDigits ? `Table ${rawDigits}` : (table || "Table 1");
+
+  useEffect(() => {
+    tableStore.setTableNumber(formatted);
+  }, [formatted]);
+
+  // Synchronously update store before rendering redirect
+  tableStore.setTableNumber(formatted);
+
+  return <Navigate to="/" replace />;
+}
